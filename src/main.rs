913d230240a9d59c7a54ea54a2d00b9bd02e50c2
ratwolfzero <ratwolf@duck.com>
2023-10-04 21:33:02 +0200

    modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -136,7 +136,6 @@ fn main() {
         println!("odd: {}", odd);
         println!();
         println!()
-        println!()
     } else {
         println!("Invalid input. Please enter a valid positive integer or a valid expression like '2^199-1'.");
     }
