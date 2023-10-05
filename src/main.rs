6769b45dab9e1317b48ba715f24b0d8b95ba0db4
ratwolfzero <ratwolf@duck.com>
2023-10-05 04:03:08 +0200

    modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -100,7 +100,7 @@ fn main() {
     //call Function to parse the input value
     if let Some(parsed_input) = parse_input(input_value) {
         // Continue with the parsed input
-        println!("Parsed input: {:?}", parsed_input);
+        println!("Parsed input: {}", parsed_input);
         println!();
 
         let output_file_path =
