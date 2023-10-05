01d26b32583dc440482269778f1d7d0905f8f857
ratwolfzero <ratwolf@duck.com>
2023-10-05 20:17:26 +0200

    modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -153,6 +153,7 @@ fn main() {
         println!("even (white): {}", even);
         println!("odd (yellow): {}", odd);
         println!();
+        println!();
         println!()
     } else {
         println!("Invalid input. Please enter a valid positive integer or a valid expression like '2^199-1'.");
